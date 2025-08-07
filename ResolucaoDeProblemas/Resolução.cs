eabe5233a8787a779de0bf2a7273c0f2234d39d5
Nelson Dos Reis Gomes Souza <nelsondosreisgomessouza@gmail.com>
2025-08-06 23:46:47 -0300

    feat(resolucao): adicionar pasta de resolucao de problemas]

---
+++ ResolucaoDeProblemas/Resolução.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+//class HelloWorld
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Hello, World!");
+//    }
+//}  
+
+//class OrderNumber
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Digite o número:");
+//        int number = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("O número digitado foi: " + number);
+//    }
+//}
+
+//class Sum {     static void Main()
+//    {
+//        Console.WriteLine("Digite o primeiro número:");
+//        int number1 = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("Digite o segundo número:");
+//        int number2 = Convert.ToInt32(Console.ReadLine());
+//        int sum = number1 + number2;
+//        Console.WriteLine("A soma dos dois números é: " + sum);
+//    }
+//}
+
+//class Average
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Digite a primeira nota:");
+//        float grade1 = float.Parse(Console.ReadLine());
+//        Console.WriteLine("Digite a segunda nota:");
+//        float grade2 = float.Parse(Console.ReadLine());
+//        Console.WriteLine("Digite a terceira nota:");
+//        float grade3 = float.Parse(Console.ReadLine());
+//        Console.WriteLine("Digite a quarta nota:");
+//        float grade4 = float.Parse(Console.ReadLine());
+//        float average = (grade1 + grade2 + grade3 + grade4) / 4;
+//        Console.WriteLine("A média das três notas é: " + average);
+//    }
+//}
+
